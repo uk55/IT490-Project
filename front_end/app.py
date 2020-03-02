9fd59de5b35e1bc23027f4493c526d654fc8201b
Sagar Patel <sagar.patel1950@yahoo.com>
2020-03-01 20:18:53 -0500

    Added Login Page with admin as username and password

--- front_end/app.py
+++ front_end/app.py
@@ -1,7 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for, request
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-        return 'Hello, World!'
+def home():
+        return 'Welcome To IT490-Group 7'
 
+# Route for handling the login page logic
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials!!!'
+        else:
+            return redirect(url_for('home'))
+    return render_template('login.html', error=error)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
