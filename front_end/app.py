0eb937b6a03c59aefefbfa1ab7720d2573089601
Sagar Patel <sagar.patel1950@yahoo.com>
2020-03-03 00:46:02 -0500

    Added flask code to require authentication in order to view the webpage.

--- front_end/app.py
+++ front_end/app.py
@@ -1,9 +1,30 @@
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, request, session, flash
+from functools import wraps
 app = Flask(__name__)
 
+app.secret_key="my precious"
+
+
+def login_required(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash('You need to login first.')
+            return redirect(url_for('login'))
+    return wrap
+
 @app.route('/')
+@login_required
 def home():
-        return 'Welcome To IT490-Group 7'
+        # return 'Welcome To IT490-Group 7'
+        return render_template('index.html')
+
+@app.route('/welcome')
+@login_required
+def welcome():
+    return render_template('welcome.html')
 
 # Route for handling the login page logic
 @app.route('/login', methods=['GET', 'POST'])
@@ -13,8 +34,17 @@ def login():
         if request.form['username'] != 'admin' or request.form['password'] != 'admin':
             error = 'Invalid Credentials!!!'
         else:
+            session['logged_in']=True
+            flash("You were just Logged in!")
             return redirect(url_for('home'))
     return render_template('login.html', error=error)
+
+@app.route('/logout')
+@login_required
+def logout():
+    session.pop('logged_in', None)
+    flash("You have been successfully Logged Out!")
+    return redirect(url_for('welcome'))
 
 
 # Route for handling the Signup page logic
