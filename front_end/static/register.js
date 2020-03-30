291cfa74ea5ef9a2d003f2059da7e43bf574aa31
Sagar Patel <sagar.patel1950@yahoo.com>
2020-03-29 20:57:18 -0400

    Added the static folder for the templets to register

---
+++ front_end/static/register.js
@@ -0,0 +1,16 @@
+$("document").ready(function(){
+    $("#send").click(function(){
+        var name = $("#name").val();
+        var password = $("#password").val();
+
+            $.ajax({
+            url: "http://localhost:5000/register",
+            type: "POST",
+            contentType: "application/json",
+            data: JSON.stringify({"message": "registered successfully"})     
+           }).done(function(data)
+            {
+            console.log(data);   
+             }); 
+        });
+    });
